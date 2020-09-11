13ef64f65f55a697e23cdfb884a44b2a8c95cfb9
xiaoqi25478 <xiaoqi25478@foxmail.com>
2020-09-11 08:10:27 +0800

    update

--- Machine Learning/LinearRegression/LinearRegression.py
+++ Machine Learning/LinearRegression/LinearRegression.py
@@ -3,6 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from myPreProcess import loadTxtAndCsvData,loadNpyData,featureNormalize,plot2D,plotJ
 from myML import computerCost,gradientDescent
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
 
 class LinearRegression:
     def __init__(self,file_dir,alpha,num_iters = 400):
@@ -10,7 +12,7 @@ class LinearRegression:
         self.num_iters = num_iters
         self.f_dir = file_dir
 
-    def fit(self):
+    def numpy_fit(self):
         """训练函数
         """
         print('step1:loading data')
@@ -21,8 +23,7 @@ class LinearRegression:
         col = data.shape[1]
 
         X,mu,sigma = featureNormalize(X)
-        plot2D(X)
-
+        #plot2D(X)
         X = np.hstack((np.ones((m,1)),X)) # 在X前面加一列1 
 
         print('step2:running gradient descent')
@@ -31,7 +32,31 @@ class LinearRegression:
         theta,J_history = gradientDescent(X,y,theta,self.alpha,self.num_iters)
         plotJ(J_history,self.num_iters)
         return mu,sigma,theta
-        
+
+    def sklearnFit(self):
+        """使用sklearn库进行学习
+        """
+        data = loadTxtAndCsvData(self.f_dir,',',dataType=np.float64)  #数据读取
+        X = np.array(data[:,0:-1],dtype=np.float64)
+        y = np.array(data[:,-1],dtype=np.float64)
+
+        #归一化操作
+        scaler = StandardScaler()
+        scaler.fit(X)
+        x_train = scaler.transform(X)
+        x_test  = np.array([1650,3])
+        x_test  = scaler.transform(x_test.reshape(1,-1))
+
+        #线性模型模拟
+        model = linear_model.LinearRegression()
+        model.fit(x_train,y)
+
+        #预测结果
+        result = model.predict(x_test)
+        print(model.coef_)       # Coefficient of the features 决策函数中的特征系数
+        print(model.intercept_)  # 又名bias偏置,若设置为False，则为0
+        print(result)            # 预测结果   
+
     def predict(self,mu,sigma,theta):
         """预测
         """
@@ -45,10 +70,11 @@ class LinearRegression:
         return result
 
 if __name__ == "__main__":
-    lr = LinearRegression("data.txt",0.01,1000)
-    mu,sigma,theta = lr.fit()
+    print('*************numpy machine learning***********')
+    lr = LinearRegression("./data.txt",0.01,600)
+    mu,sigma,theta = lr.numpy_fit()
     print ("theta:\n",theta)
     print ("predict:%f"%lr.predict(mu, sigma, theta))
 
-
-        
+    print('*************sklearn machine learning***********')
+    lr.sklearnFit()
