fccb061450469e9d7a851f820886b4e4c9dc8c58
xiaoqi25478 <xiaoqi25478@foxmail.com>
2020-09-17 21:47:57 +0800

    update

---
+++ Machine Learning/kaggle/Titanic Machine Learning from Disaster/Titanic Data Science Solutions.py
@@ -0,0 +1,10 @@
+"""
+参考链接:https://www.kaggle.com/startupsci/titanic-data-science-solutions
+1.Question or problem definition.
+2.Acquire training and testing data.
+3.Wrangle, prepare, cleanse the data.
+4.Analyze, identify patterns, and explore the data.
+5.Model, predict and solve the problem.
+6.Visualize, report, and present the problem solving steps and final solution.
+7.Supply or submit the results.
+"""
